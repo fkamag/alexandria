ff6826a25b6a76e1a76d2ec5d1c9041b902583cf
fkamag <fabiokamagian@gmail.com>
2024-04-22 19:04:52 -0300

    feat BookDetail - entity/repository

--- src/main/java/com/betrybe/alexandria/entity/Book.java
+++ src/main/java/com/betrybe/alexandria/entity/Book.java
@@ -1,9 +1,11 @@
 package com.betrybe.alexandria.entity;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 
 @Entity
@@ -14,6 +16,8 @@ public class Book {
   private Long id;
   private String title;
   private String genre;
+  @OneToOne(mappedBy = "book", cascade = CascadeType.ALL)
+  private BookDetail detail;
 
   public Book() {
   }
@@ -45,5 +49,13 @@ public class Book {
 
   public void setGenre(String genre) {
     this.genre = genre;
+  }
+
+  public BookDetail getDetail() {
+    return detail;
+  }
+
+  public void setDetail(BookDetail detail) {
+    this.detail = detail;
   }
 }
